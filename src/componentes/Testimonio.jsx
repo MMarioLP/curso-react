3f3d75788ca067604c1222c17f29b144c6e26266
MMarioLP <am4063228@gmail.com>
2023-02-17 21:21:37 -0600

    cambios

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -5,14 +5,18 @@ import '../hojas-de-estilo/Testimonio.css';
   return (
     <div className='contenedor-testimonio'>
       <img className='imagen-testimonio' 
-      src={require('../imagenes/testimonio-emma.png')}
-      alt='Foto de emma'/>
+      src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+      alt='Foto de emma' />
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>{props.nombre} en {props.pais }</p>
-        <p className='cargo-testimonio'>Ineniera de Software en spotify</p>
-        <p className='texto-testimonio'>"Siempre he tenido problemas para aprender JavaScript. Tomé muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como un ingeniero de software en Spotify".</p>
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais }</p>
+        <p className='cargo-testimonio'>{props.cargo} de Software en <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>"{props.testimonio}".</p>
       </div>
     </div>
   );
 }  
 export default Testimonio;
+
+//lograr que eel etxto testimonio aparezca en negrita
+// que el alt de la descripcion corresponda a la imagen dinamicamente de acuredo a la imagen
+// guardar los valores de los props en un archivo por separado en un solo objeto de javascript  
